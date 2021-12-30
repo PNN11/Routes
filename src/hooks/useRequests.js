b20acbf11abef2a387e646b384b3b8cc2f456dbb
PNN11 <pavlik.nedvetsky@gmail.com>
2021-12-30 14:24:57 +0300

    fix useRequests func

--- src/hooks/useRequests.js
+++ src/hooks/useRequests.js
@@ -1,23 +1,18 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 
 const useRequests = (asyncRequestFunctions) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const functions = useMemo(
-    () => asyncRequestFunctions.map((func) => func()),
-    [asyncRequestFunctions]
-  );
-
   useEffect(() => {
     setLoading(true);
 
-    Promise.all(functions)
+    Promise.all(asyncRequestFunctions.map((func) => func()))
       .then((data) => setData(data))
       .catch(() => setError(true))
       .finally(() => setLoading(false));
-  }, [functions]);
+  }, [asyncRequestFunctions]);
   return { data, loading, error };
 };
 
